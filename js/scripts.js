564d396c9d8952b737099baff0e0b5d9341e1c77
Josh Gearheart and Michael Donovan <michael@>
2018-08-06 16:46:12 -0700

    Initial commit - lots of logic

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+var userSentence = copySentence = prompt("Enter your sentence:")
+
+$(document).ready(function(){
+  $("#left-button").click(function() {
+    cipher(userSentence)
+  });
+  $("#right-button").click(function() {
+    decipher(userSentence)
+  });
+  printSentence(userSentence)
+})
+
+function cipher(string) {
+  console.log("ciphering " + string);
+  halfIndexLetter()
+
+};
+function decipher(string) {
+  console.log("deciphering " + string);
+  printSentence(userSentence)
+};
+function printSentence(string) {
+  $("#sentence-display").html(string);
+}
+
+// business logic
+function firstLast() {
+  var firstLetter = copySentence[0].toUpperCase();
+  var lastLetter = copySentence[copySentence.length-1].toUpperCase();
+  copySentence = (firstLetter+lastLetter)
+  return copySentence
+}
+
+function reverseOrder() {
+  var splitArray = copySentence.split("");
+  console.log("split to " + splitArray);
+  var reverseArray = splitArray.reverse();
+  console.log("reversed to " + reverseArray);
+  var joined = reverseArray.join("");
+  console.log("joined to " + joined);
+  copySentence = joined;
+  return copySentence;
+}
+
+function pasteOn() {
+  var addendum = reverseOrder(firstLast());
+  copySentence = (userSentence + addendum);
+  return copySentence
+}
+
+function halfIndexLetter() {
+  var halfSentenceIndex = Math.floor(userSentence.length/2);
+  var indexLetter = userSentence[halfSentenceIndex]
+  copySentence = (indexLetter + userSentence)
+  console.log(copySentence)
+  return copySentence
+}
